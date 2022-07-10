4dc3ea67e0ff3b82e3131b5586ef884fdcd8f7c2
Tejas <tejas75o25@gmail.com>
2022-07-10 11:22:26 +0530

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name="kct_pygame_utils",
+    version="1.0.0",
+    description="A simple python library that provides utilities for pygame games",
+    long_description=open("README.md").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    url="https://github.com/KidCoderT/my_first_package",
+    authors="KidCoderT",
+    author_email="tejas75o25@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="",
+    packages=find_packages(),
+    install_requires=["pygame"],
+)
