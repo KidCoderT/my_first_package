2890c134b8ce2b07112969039075c281cb9371c8
Tejas <tejas75o25@gmail.com>
2022-07-10 12:07:10 +0530

    changed name

--- setup.py
+++ setup.py
@@ -6,11 +6,36 @@ classifiers = [
     "Operating System :: OS Independent",
 ]
 
+long_description = """
+# KCT Pygame Utils
+
+A set of handy pygame functions and classes to help with in making you next new game.
+
+features include
+
+1. Contains Utils that can handle image scaling, opacity, outline and sprite sheets
+2. Contains Classes that can be extended to create Particle Systems
+3. Contains methods to easily make a scrolling background
+4. Contains methods that can help with text
+
+This is my very first module built with the python for geeks book.
+
+If you want references on how to use this module go to the [examples folder](https://github.com/KidCoderT/my_first_package/tree/master/kct_pygame_utils/examples) and here it contains 5 different tests each showing a use case of one or more of the function and methods provided by the package.
+
+
+Change Log                @KCT
+==============================
+
+0.0.1 (10/07/2020)
+-------------------
+- Initial release
+"""
+
 setup(
-    name="kct_pygame_utils",
+    name="kctpygameutils",
     version="1.0.1",
     description="A simple python library that provides utilities for pygame games",
-    long_description=open("README.md").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    long_description=long_description,
     url="https://github.com/KidCoderT/my_first_package",
     authors="KidCoderT",
     author_email="tejas75o25@gmail.com",
